aae86b5b22c62ece7ee6d0c1a16679319a6a0ba9
Philip Davey <philip.davey@ucalgary.ca>
2022-11-24 16:49:37 -0700

    Created project and set up fxml file.

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/ApplicationView.fxml"));
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Philip's Calorie Counter");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
