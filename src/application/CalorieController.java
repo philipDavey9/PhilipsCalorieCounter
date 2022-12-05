c7752d20a03bcb5ec20a8d17688237159cd48a17
Philip Davey <philip.davey@ucalgary.ca>
2022-12-05 12:42:18 -0700

    Added scene for users to add meals and others to calorie total.

--- src/application/CalorieController.java
+++ src/application/CalorieController.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -32,6 +33,12 @@ import javafx.scene.layout.*;
 	     
 	     @FXML 
 	     private Slider exerciseIntensity;
+	     
+	     @FXML 
+	     private ChoiceBox<Integer> mealNumber;
+	     
+	     @FXML 
+	     private ChoiceBox<Integer> otherNumber;
 
 	    @FXML
 	    void enterInputScreen(ActionEvent inputScreenEvent) {
@@ -56,13 +63,79 @@ import javafx.scene.layout.*;
 	    }
 	    
 	    @FXML 
-	    void enterOthers(ActionEvent event) {
+	    void enterOthers(ActionEvent othersEvent) {
+Scene mainScene = applicationStage.getScene();
 	    	
+	    	int numberOfOtherItems = otherNumber.getValue();
+	    	int rowsCreated = 0;
+	    	VBox othersContainer = new VBox();
+	    	ArrayList<TextField> othersList = new ArrayList<TextField>();
+	    	ArrayList<TextField> othersPortion = new ArrayList<TextField>();
+	    	
+	    	while(rowsCreated < numberOfOtherItems) {
+	    		
+	    	HBox enterScreenContainer = new HBox();
+	    	Label mealLabel = new Label("Enter Other Item:");
+	    	TextField mealText = new TextField();
+	    	Label mealLabel2 = new Label("Serving Size:");
+	    	TextField mealText2 = new TextField();
+	    	Label lastLabel2 = new Label("Servings");
+	    	
+	    	othersList.add(mealText);
+	    	othersPortion.add(mealText2);
+	    	
+	    	enterScreenContainer.getChildren().addAll(mealLabel, mealText, mealLabel2, mealText2, lastLabel2);
+	    	rowsCreated++;
+	    	
+	    	othersContainer.getChildren().add(enterScreenContainer);
+	    	}
+	    	
+	    	Button enterDoneButton = new Button("Done");
+	    	enterDoneButton.setOnAction(doneEvent -> addOtherCalories(mainScene, othersList, othersPortion));
+	    	othersContainer.getChildren().add(enterDoneButton);
+	    	Scene enterScreenScene = new Scene(othersContainer);
+	    	applicationStage.setScene(enterScreenScene);
+	    }
+	    void addOtherCalories(Scene mainScene, ArrayList<TextField> othersList, ArrayList<TextField> othersPortion) {
+	    	applicationStage.setScene(mainScene);
 	    }
 	    
 	    @FXML 
-	    void enterMeals(ActionEvent event) {
+	    void enterMeals(ActionEvent enterEvent) {
+	    	Scene mainScene = applicationStage.getScene();
 	    	
+	    	int numberOfMeals = mealNumber.getValue();
+	    	int rowsCreated = 0;
+	    	VBox mealContainer = new VBox();
+	    	ArrayList<TextField> mealList = new ArrayList<TextField>();
+	    	ArrayList<TextField> mealPortion = new ArrayList<TextField>();
+	    	
+	    	while(rowsCreated < numberOfMeals) {
+	    		
+	    	HBox enterScreenContainer = new HBox();
+	    	Label mealLabel = new Label("Enter Meal:");
+	    	TextField mealText = new TextField();
+	    	Label mealLabel2 = new Label("Serving Size:");
+	    	TextField mealText2 = new TextField();
+	    	Label lastLabel2 = new Label("Servings");
+	    	
+	    	mealList.add(mealText);
+	    	mealPortion.add(mealText2);
+	    	
+	    	enterScreenContainer.getChildren().addAll(mealLabel, mealText, mealLabel2, mealText2, lastLabel2);
+	    	rowsCreated++;
+	    	
+	    	mealContainer.getChildren().add(enterScreenContainer);
+	    	}
+	    	
+	    	Button enterDoneButton = new Button("Done");
+	    	enterDoneButton.setOnAction(doneEvent -> addMealCalories(mainScene, mealList, mealPortion));
+	    	mealContainer.getChildren().add(enterDoneButton);
+	    	Scene enterScreenScene = new Scene(mealContainer);
+	    	applicationStage.setScene(enterScreenScene);
+	    }
+	    void addMealCalories(Scene mainScene, ArrayList<TextField> mealList, ArrayList<TextField> mealPortion) {
+	    	applicationStage.setScene(mainScene);
 	    }
 
 	   
